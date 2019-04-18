4f1e2aaffba0efc2c3c41d2fab0b4401b7d124d0
bohdans <0000xffff@gmail.com>
2019-04-18 18:48:24 +0300

    initial project

---
+++ CoordinatorExample/CoordinatorExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CoordinatorExample
+//
+//  Created by Bogdan Sasko on 4/18/19.
+//  Copyright © 2019 vinso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
